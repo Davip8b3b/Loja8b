4c12c8f1ef50fadc31d7b55dba0015c77021f1aa
* <davip8a3a@gmail.com>
2023-04-22 19:32:47 -0300

    Ativar debug e adição do arquivo config

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config.from_object("config")
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
